909b6fc56872e35860dedb3dc2ea44ea3a9c7427
DevJebez <jebezoswald.2965@gmail.com>
2025-04-18 14:49:11 +0530

    Second largest element in an array

--- GFG Challenge/second_largest.java
+++ GFG Challenge/second_largest.java
@@ -1,5 +1,19 @@
 public class second_largest{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if(arr.length < 2){
+            return -1;
+        }
+        int max = -1;
+        int second_max = -1;
+        for(int num : arr){
+            if(num>max){
+                second_max = max; 
+                max = num;
+            }
+            else if(num < max && num > second_max){
+                second_max = num;
+            }
+        }
+        return second_max;
     }
 }
