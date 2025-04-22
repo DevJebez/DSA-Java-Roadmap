40f8937c17ca76073fa3a6672910e49e78d850c8
DevJebez <jebezoswald.2965@gmail.com>
2025-04-22 16:55:16 +0530

    Boyer Moore alg

--- 03_dsa_foundations/boyer_moore_alg.java
+++ 03_dsa_foundations/boyer_moore_alg.java
@@ -7,6 +7,17 @@ When you meet a different one, you reduce the vote.
 
 Since the majority element appears more than all others combined, 
 it will still remain the final candidate.
+
+So why 1 element ?? 
+This comes from the piegon hole principle.
+Lets say there are two elements A, B
+We are finding all the elements with frequency > n/2
+Count(A) > n/2
+Count(B) > n/2 
+This implies 
+Count(A) + Count(B) > n
+This is not true so, we 
+there is one element which has n/2 occurences
 */ 
 
 public class boyer_moore_alg{
@@ -34,7 +45,7 @@ public class boyer_moore_alg{
     }
 
     public static void main(String[] args){
-        int[] arr = {3,1,3,1,1};
+        int[] arr = {3,1,3,1,6,1};
         System.out.println("majority_element : " + majority_element(arr));
  
     }
