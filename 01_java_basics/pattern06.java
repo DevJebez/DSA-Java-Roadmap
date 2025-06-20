f0e07b83b7db4b687d2b39c48a97b98a5028776e
DevJebez <jebezoswald.2965@gmail.com>
2025-06-20 11:01:09 +0530

    Star pyramid

---
+++ 01_java_basics/pattern06.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class pattern06{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  N:");int N = sc.nextInt();
+        for(int i = 0; i < N; i++){
+            for(int j = 0 ;j < N-i-1; j++ ){
+                System.out.print(" ");
+            }
+            for(int j = 0 ; j < 2*i + 1 ; j++ ){
+                System.out.print("*");  
+            }
+            for(int j = 0 ; j <= N-i-1 ; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
