23ea881a44a49aa9d8b619b83685b0444b20d9fc
DevJebez <jebezoswald.2965@gmail.com>
2025-08-06 09:49:13 +0530

    increasing number traingle pattern

--- 01_java_basics/pattern13.java
+++ 01_java_basics/pattern13.java
@@ -1,3 +1,22 @@
+/*
+Examples:
+
+Input Format: N = 3
+Result: 
+1
+2 3
+4 5 6
+
+Input Format: N = 6
+Result:   
+1
+2  3
+4  5  6
+7  8  9  10
+11  12  13  14  15
+16  17  18  19  20  21
+*/
+
 import java.util.Scanner;
 public class pattern13{ 
     public static void main(String args[]){
