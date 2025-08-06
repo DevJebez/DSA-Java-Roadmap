5017603bd10d35404612b39680d53ddcd0727ac7
DevJebez <jebezoswald.2965@gmail.com>
2025-08-06 08:39:55 +0530

    inverted numbered right pyramid

--- 01_java_basics/pattern06.java
+++ 01_java_basics/pattern06.java
@@ -1,17 +1,30 @@
+/*
+Examples:
+
+Input Format: N = 3
+Result: 
+1 2 3
+1 2
+1
+
+Input Format: N = 6
+Result:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+*/
+
 import java.util.Scanner;
 public class pattern06{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter  N:");int N = sc.nextInt();
-        for(int i = 0; i < N; i++){
-            for(int j = 0 ;j < N-i-1; j++ ){
-                System.out.print(" ");
-            }
-            for(int j = 0 ; j < 2*i + 1 ; j++ ){
-                System.out.print("*");  
-            }
-            for(int j = 0 ; j <= N-i-1 ; j++){
-                System.out.print(" ");
+        System.out.print("Enter a number: ");int n = sc.nextInt();
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1 ;j <= i ; j++ ){
+                System.out.print(j);
             }
             System.out.println();
         }
