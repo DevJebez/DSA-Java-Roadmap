d1fcf54d2b675d848b42cd813ac210314f6d1019
DevJebez <jebezoswald.2965@gmail.com>
2025-08-09 12:05:35 +0530

    Symmetric void pattern

--- 01_java_basics/pattern18.java
+++ 01_java_basics/pattern18.java
@@ -1,3 +1,25 @@
+/*
+Here, N = 5.
+
+Examples:
+
+Input Format: N = 3
+Result: 
+C
+B C
+A B C
+
+Input Format: N = 6
+Result:   
+F
+E F
+D E F
+C D E F
+B C D E F
+A B C D E F
+
+*/
+
 import java.util.Scanner;
 public class pattern18{
     public static void main(String args[]){
