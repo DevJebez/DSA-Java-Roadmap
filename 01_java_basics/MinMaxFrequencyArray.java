1bcc3632922126bc25a3c0fa802e9307a81fd2a4
DevJebez <jebezoswald.2965@gmail.com>
2025-09-04 09:26:56 +0530

    Modified changes

--- 01_java_basics/MinMaxFrequencyArray.java
+++ 01_java_basics/MinMaxFrequencyArray.java
@@ -3,7 +3,8 @@ public class MinMaxFrequencyArray{
     public static void main(String[] args){
         int[] arr = {10,20,10,50,60,3};
         int n = arr.length;
-        MinMaxCountFreq(arr,n);
+        System.out.println("Using Brute Force");MinMaxCountFreq(arr,n);
+        System.out.println("Optimal approach");OptimalApproach(arr,n);
     }
     //Time Complexity: O(N*N), where N = size of the array. We are using the nested loop to find the frequency.
     //Space Complexity:  O(N), where N = size of the array. It is for the visited array we are using.
@@ -37,5 +38,34 @@ public class MinMaxFrequencyArray{
         System.out.println("Lowest frequency element:"+minelement);
     }
     //optimal approach using hashmap in java
-    
+    static void OptimalApproach(int arr[], int n){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< n; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        //find the max and min frequency
+        int maxcount = 0;
+        int mincount = n;
+        int minelement = 0;
+        int maxelement = 0;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int count = entry.getValue();
+            int element = entry.getKey();
+            if(count > maxcount){
+                maxcount = count;
+                maxelement = element;
+            }
+            if(count < mincount){
+                mincount = count;
+                minelement = element;
+            }
+        }
+        System.out.println("Highest frequency element:"+maxelement);
+        System.out.println("Lowest frequency element:"+minelement);
+    }
 }
--- SkillUp Challenge/NineDivisors.java
+++
@@ -1,7 +0,0 @@
-public class NineDivisors{
-    public static void main(String args[]){
-        int n = 100 ;
-        int count = 0;
-        
-    }
-}
