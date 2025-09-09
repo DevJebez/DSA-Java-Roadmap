87d9c8d7ab85597fe2283339efed76c9c93fc853
DevJebez <jebezoswald.2965@gmail.com>
2025-09-09 18:00:28 +0530

    optimal

--- Strivers/arrays/easy/removeduplicates.java
+++ Strivers/arrays/easy/removeduplicates.java
@@ -18,10 +18,22 @@ public class removeduplicates{
         }
         return k;
     }
+    static int optimal(int[] arr, int n){
+        int l = 1;
+        int r = 1;
+        while(r < n){
+            if(arr[r] !=  arr[r-1]){
+                arr[l++] = arr[r];
+            }
+            r++;
+        }
+        return l;
+    }
     public static void main(String args[]){
         int[] arr = {1,1,1,2,2,2,3,3,3,3};
         int n = arr.length;
         System.out.println(bruteforce(arr, n));
+        System.out.println(optimal(arr,n));
 
     }
 }
