7ddbf775d0b039abade5cb18a1dbf4da01f73da2
DevJebez <jebezoswald.2965@gmail.com>
2025-09-09 08:07:15 +0530

    Check sorted

---
+++ Strivers/arrays/easy/checksorted.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class checksorted{
+    static boolean bruteforce(int[] arr, int n){
+        for(int i = 0 ; i<n;i++){
+            for(int j = i+1; j<n ; j++){
+                if(arr[j] < arr[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+    static boolean optimal(int[] arr, int n){
+        for(int i = 1; i < n;i++){
+            if (arr[i] < arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        //to check whether the given array is sorted in ascending order or not
+        int[] arr = {1,2,3,4,6,6};
+        int n= arr.length;
+        if(optimal(arr,n) && bruteforce(arr,n)) System.out.println("Sorted");
+        else System.out.println("Not sorted");
+        
+    }
+}
