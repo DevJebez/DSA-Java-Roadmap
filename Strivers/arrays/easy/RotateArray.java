a2ade14632071cd785268626a8b0f7c9a662f200
DevJebez <jebezoswald.2965@gmail.com>
2025-09-10 20:59:16 +0530

    Rotate an array by k elements

--- Strivers/arrays/easy/RotateArray.java
+++ Strivers/arrays/easy/RotateArray.java
@@ -1,24 +1,27 @@
 import java.util.*;
-public class leftrotate{
+public class RotateArray{
     public static void printArray(int[] arr){
         for(int num : arr){
             System.out.print(num+" ");
         }
     }
-    public static void optimal(int[] arr, int n){
-        int temp = arr[0];
-        int j = 0;
-        for(int i = 1; i< n;i++){
-            arr[j] = arr[i];
+    private static void rotate(int[] arr, int n, int k){
+        k = k % n; //  to handle k > n 
+        int j = n - k;
+        for(int i = 0; i< k;i++){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             j++;
         }
-        arr[n-1] = temp;
         printArray(arr);
     }
+
     public static void main(String args[]){
         int[] arr = {1,2,3,4,5,6};
         int n = arr.length;
-        optimal(arr,n);
+        int k = 7;
+        rotate(arr,n,k);
         //brute force solution uses O(N) extra space. A new array is used to store the elements.
     }
 }
