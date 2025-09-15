82177e62601464dc08b70b25e2681ada8434526a
DevJebez <jebezoswald.2965@gmail.com>
2025-09-15 10:57:55 +0530

    Union of two sorted arrays

--- Strivers/arrays/easy/union.java
+++ Strivers/arrays/easy/union.java
@@ -1,11 +1,3 @@
-/*
-
-We are asked to find the union of two sorted arrays
-1. Combine both array 
-2. Remove the duplicates 
-3. Return the array
-*/
-
 import java.util.*;
 
 public class union {
@@ -13,71 +5,85 @@ public class union {
         for (int num : arr) {
             System.out.print(num + " ");
         }
+        System.out.println();
     }
-    private static int[] twopointers(int[] arr1, int[] arr2){
-        int i = 0;
-        int j = 0;
+
+    private static int[] twopointers(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
         ArrayList<Integer> union = new ArrayList<>();
-        while(i < arr1.length && j < arr2.length){
-            if(arr1[i] < arr2[j]){
-                if(union.size() == 0 || union.get(union.size()-1) != arr1[i]){
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
                     union.add(arr1[i]);
                 }
                 i++;
-            }
-            else{
-                if(arr2[i] < arr1[j]){
-                    if(union.size() == 0 || union.get(union.size()-1) != arr2[i]){
-                        union.add(arr2[i]);
-                    }
-                j++;
+            } else if (arr2[j] < arr1[i]) {
+                if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
+                    union.add(arr2[j]);
                 }
+                j++;
+            } else { // arr1[i] == arr2[j]
+                if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+                j++;
             }
         }
-        while(i < arr1.length){
-            if(union.get(union.size() - 1) != arr1[i]){
+
+        while (i < arr1.length) {
+            if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
                 union.add(arr1[i]);
             }
+            i++;
         }
-        while(j < arr2.length){
-            if(union.get(union.size()-1)!= arr2[j]){
+
+        while (j < arr2.length) {
+            if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
                 union.add(arr2[j]);
             }
+            j++;
         }
+
         int[] result = new int[union.size()];
-        for (i = 0; i < union.size(); i++) {
-            result[i] = union.get(i);
+        for (int k = 0; k < union.size(); k++) {
+            result[k] = union.get(k);
         }
         return result;
     }
-    private static int[] bruteforce(int[] arr1, int[] arr2) {
-        int n1 = arr1.length;
-        int n2 = arr2.length;
 
+    private static int[] bruteforce(int[] arr1, int[] arr2) {
         ArrayList<Integer> union = new ArrayList<>();
-        for (int i = 0; i < n1; i++) {
-            if (!union.contains(arr1[i])) {  // remove duplicates from arr1
+        for (int i = 0; i < arr1.length; i++) {
+            if (!union.contains(arr1[i])) {
                 union.add(arr1[i]);
             }
         }
-        for (int j = 0; j < n2; j++) {
-            if (!union.contains(arr2[j])) {  // remove duplicates from arr2
+        for (int j = 0; j < arr2.length; j++) {
+            if (!union.contains(arr2[j])) {
                 union.add(arr2[j]);
             }
         }
-        int[] result = new int[union.size()];
-        for (i = 0; i < union.size(); i++) {
-            result[i] = union.get(i);
-        }
 
-        return result; // return int[]
+        int[] result = new int[union.size()];
+        for (int k = 0; k < union.size(); k++) {
+            result[k] = union.get(k);
+        }
+        return result;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {2, 3, 3, 4, 5};
-        int[] result = bruteforce(arr1, arr2); // Time complexity 
+
+        int[] result = bruteforce(arr1, arr2);
         int[] optimal_result = twopointers(arr1, arr2);
+
+        System.out.println("Bruteforce result:");
         printArray(result);
+
+        System.out.println("Two-pointers result:");
+        printArray(optimal_result);
     }
 }
